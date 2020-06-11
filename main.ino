0b2f531920fe0266862bb8c719ac837097e21040
jcombitsis17 <jcombitsis17@gmail.com>
2020-06-10 17:23:18 -0700

    double press button (need fusion code @ line ~114)

--- main.ino
+++ main.ino
@@ -23,11 +23,19 @@ const size_t startRotateMessageSize = quaternionMessageSize + sizeof(float);
 uint8_t startRotateMessage[startRotateMessageSize];
 const size_t velocityMessageSize = sizeof(float) * 2;
 uint8_t velocityMessage[velocityMessageSize];
+const size_t doubleClickMessageSize = sizeof(float) * 2;
+uint8_t doubleClickMessage[doubleClickMessageSize];
+
+unsigned long startTime = 0;
+const int doubleClickThresh = 600; // limit of 600 ms to wait for 2nd button click (release)
+int doubleClickButton;
+int buttonState = 0;
 
 BLEService bleService("0f958eb9-b8bb-40e3-91b1-54281cabe755"); // https://www.uuidgenerator.net/
 BLECharacteristic rotateChar("06d66869-9fc1-4141-970d-dd5f6088723a", BLERead | BLENotify, quaternionMessageSize);
 BLECharacteristic startRotateChar("d9acf2e8-0f26-4707-94eb-091afc18e952", BLERead | BLEIndicate, startRotateMessageSize);
 BLECharacteristic velocityChar("4c3a0eec-d518-4e1e-a8c3-664111eb4d47", BLERead | BLENotify, velocityMessageSize);
+BLECharacteristic doubleClickChar("343056fa-14a3-4b67-9b05-37045461ebd2", BLERead | BLENotify, doubleClickMessageSize);
 
 BLEDevice central;
 
@@ -58,6 +66,7 @@ void setup() {
     bleService.addCharacteristic(rotateChar);
     bleService.addCharacteristic(startRotateChar);
     bleService.addCharacteristic(velocityChar);
+    bleService.addCharacteristic(doubleClickChar);
     BLE.addService(bleService);
     BLE.advertise();
 
@@ -77,6 +86,37 @@ void loop() {
 
     if (!central.connected()) {
         central = BLE.central();
+    }
+
+    // reset double click timer
+    if (millis() - startTime > doubleClickThresh)
+      buttonState = 0;
+
+    // search for double click
+    switch (buttonState) {
+      case 0: // press 1
+        if (!digitalRead(ROTATE_BUTTON) || !digitalRead(TRANSLATE_BUTTON)) {
+          doubleClickButton = !digitalRead(ROTATE_BUTTON) ? ROTATE_BUTTON : TRANSLATE_BUTTON;
+          startTime = millis();
+          buttonState = 1;
+        }
+        break;
+      case 1: // release 1
+        if (digitalRead(doubleClickButton))
+          buttonState = 2;
+        break;
+      case 2: // press 2
+        if (!digitalRead(doubleClickButton))
+          buttonState = 3;
+        break;
+      case 3: // release 2
+        if (digitalRead(doubleClickButton)) {
+          // SUCCESS: NOTIFY/SEND DATA
+          buttonState = 0;
+        }
+      default:
+        Serial.println("um how did this happen");
+        break;
     }
 
     if (!digitalRead(ROTATE_BUTTON) != rotating) {
